22c01ecab76816671e093642e9c42be283ccf16e
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-25 09:58:45 +1100

    added toggle button to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,45 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 function Nav() {
   return (
-       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar navbar-light "> 
       <Link className="navbar-brand" to="/Home">
-        <img src="/sharing.png"></img> 
+        Sharing is Caring
       </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/aboutUs"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About Us
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="/Home"
+                className={
+                  window.location.pathname === "/" || window.location.pathname === "/about"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Home
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
-            >
-              Donate Now
+            </li>
+            {/* Items */}
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+              >
+                Donate Now
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contactUs"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Contact
+            </li>
+            {/* Contact Us */}
+            <li className="nav-item">
+              <Link
+                to="/contactUs"
+                className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+              >
+                Contact
             </Link>
-          </li>
-        </ul>
+            </li>
+            {/* About Us */}
+            <li className="nav-item">
+              <Link
+                to="/aboutUs"
+                className={
+                  window.location.pathname === "/" || window.location.pathname === "/about"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                About Us
+            </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </nav>
+    </nav >
   );
 }
 
