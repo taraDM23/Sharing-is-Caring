26cb53d014ed2680b8e64fd0b204eec8bf798547
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-25 09:57:13 +1100

    removed comments

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
+//import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
+//import { Card, Cardbtn } from "../components/Card";
 
 
 function Home() {
@@ -22,18 +23,18 @@ function Home() {
       .catch(err => console.log(err));
   };
 
-  function deleteItem(id) {
+  /* function deleteItem(id) {
     API.deleteItem(id)
       .then(res => loadItems())
       .catch(err => console.log(err));
-  }
+  } */
 
  
   return (
     <Container fluid>
       <Row>
         <Col size="md-12">
-          <Jumbotron>
+          <Jumbotron >
             <h1>Available Items</h1>
           </Jumbotron>
           {items.length ? (
@@ -45,7 +46,7 @@ function Home() {
                       {item.quantity} {item.title} posted by {item.author}
                     </strong>
                   </Link>
-                  <DeleteBtn onClick={() => deleteItem(item._id)} />
+                {/*   <DeleteBtn onClick={() => deleteItem(item._id)} /> */}
                 </ListItem>
               ))}
             </List>
