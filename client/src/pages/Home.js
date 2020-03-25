782b2cbfa9a21ab78997669cebb38c26de2b0894
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-25 21:54:33 +1100

    used meterial-ui  for col and row

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 //import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-//import { Card, Cardbtn } from "../components/Card";
+import Grid from '@material-ui/core/Grid';
+//import { Link } from "react-router-dom";
+//import { Col, Row, Container } from "../components/Grid";
+//import { List } from "../components/List";
+import MediaCard from "../components/Card";
+
 
 
 function Home() {
   const [items, setItem] = useState([])
- 
+
   useEffect(() => {
     loadItems()
   }, [])
@@ -23,39 +25,37 @@ function Home() {
       .catch(err => console.log(err));
   };
 
-  /* function deleteItem(id) {
+  function deleteItem(id) {
     API.deleteItem(id)
       .then(res => loadItems())
       .catch(err => console.log(err));
-  } */
+  }
 
- 
+
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron >
-            <h1>Available Items</h1>
-          </Jumbotron>
-          {items.length ? (
-            <List>
-              {items.map(item => (
-                <ListItem key={item._id}>
-                  <Link to={"/item/" + item._id}>
-                    <strong>
-                      {item.quantity} {item.title} posted by {item.author}
-                    </strong>
-                  </Link>
-                {/*   <DeleteBtn onClick={() => deleteItem(item._id)} /> */}
-                </ListItem>
-              ))}
-            </List>
-          ) : (
-              <h3>No Results to Display</h3>
-            )}
-        </Col>
-      </Row>
-    </Container >
+    <Grid container spacing={3} >
+      <Grid item xs={12}>
+        <Jumbotron >
+          <h1>Available Items</h1>
+        </Jumbotron>
+
+        {items.length ? (
+          <Grid container>
+            {
+              items.map(item => (
+                <Grid item xs={3}>
+                  <MediaCard key={item._id} item={item}>
+                  </MediaCard>
+                </Grid>
+              ))
+            }
+          </Grid>
+
+        ) : (
+            <h3>No Results to Display</h3>
+          )}
+      </Grid>
+    </Grid>
 
   );
 }
