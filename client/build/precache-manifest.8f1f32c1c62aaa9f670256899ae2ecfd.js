244965b8c5f85d3d92f6127f9ef72dae0e2b0987
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-28 10:08:25 +1100

    build file

---
+++ client/build/precache-manifest.8f1f32c1c62aaa9f670256899ae2ecfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "017f0d14a92e08e4cf1d",
+    "url": "/static/css/main.bfbd0879.chunk.css"
+  },
+  {
+    "revision": "017f0d14a92e08e4cf1d",
+    "url": "/static/js/main.d864bc0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1907bf131095844aaac0",
+    "url": "/static/js/2.7b51dde8.chunk.js"
+  },
+  {
+    "revision": "e0dd7101f8b4c07792cf13b91bb7906a",
+    "url": "/index.html"
+  }
+];
