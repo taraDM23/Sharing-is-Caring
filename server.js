b7feeca5ee6e063e454843f17713b8faaf2ff004
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-28 23:35:18 +1100

    updaed server with auth

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const isAuth = require("./middleware/is-auth");
 const routes = require("./routes");
 const app = express();
 
@@ -9,14 +10,11 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(isAuth);
 
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-  
-// }
 app.use(express.static("client/build"));
+
 // Add routes, both API and view
 app.use(routes);
 app.get('*',(req, res) => {
