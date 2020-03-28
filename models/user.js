5adaf344521b8049dcb46864d4e8d1296705a7d7
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-28 23:23:45 +1100

    updated user schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  user_name: { type: String, required: true },
+  //user_name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true }
 });
