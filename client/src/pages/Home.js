754d4caa03d4dba22b9c82a0e2f43ccb7f2d4639
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-28 23:25:53 +1100

    added auth to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import Grid from '@material-ui/core/Grid';
 import MediaCard from "../components/Card";
-
+import AuthContext from '../context/auth-context';
 
 
 function Home() {
+  const { token } = useContext(AuthContext);
   const [items, setItem] = useState([])
 
   useEffect(() => {
     loadItems()
+    console.log(loadItems)
   }, [])
 
   function loadItems() {
@@ -23,27 +25,28 @@ function Home() {
   };
 
   function deleteItem(id) {
+    // if user id and item it match show
     API.deleteItem(id)
       .then(res => loadItems())
       .catch(err => console.log(err));
   }
 
-
   return (
     <Grid container spacing={3} >
       <Grid item xs={12}>
         <Jumbotron >
-         {/*  <h1></h1> */}
+          <h1>Available Items</h1>
         </Jumbotron>
-        <h3 style={{ paddingBottom:15 , fontWeight: "normal" }}>Donations currently available</h3>
+
         {items.length ? (
           <Grid container>
             {
               items.map(item => (
+
                 <Grid item sm={3} xs={12} >
                   <MediaCard key={item._id} item={item}>
                   </MediaCard>
-                  <DeleteBtn onClick={() => deleteItem(item._id)} />
+                  {token && <DeleteBtn onClick={() => deleteItem(item._id)} />}
                 </Grid>
               ))
             }
