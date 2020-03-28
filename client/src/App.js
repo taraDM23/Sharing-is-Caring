5c3215b09d9aa81e7eaf74bdd67ec80162406c1b
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-28 23:27:22 +1100

    added auth to app and added auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Items from "./pages/Items";
+import AuthPage from './pages/AuthPage';
+import AuthContext from './context/auth-context';
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -10,21 +12,51 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 function App() {
+
+  const [state, setState] = useState({
+    token: null,
+    userId: null,
+  })
+
+
+  const login = (token, userId, tokenExpiration) => {
+    setState({ token: token, userId: userId })
+  }
+  const logout = (token, userId, tokenExpiration) => {
+    
+    setState({ token: null, userId: null })
+  }
+
+
   return (
     <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Items} />
-          <Route exact path="/Home" component={Home} />
-          <Route exact path="/items" component={Items} />
-          <Route exact path="/item/:id" component={Detail} />
-          <Route exact path="/aboutUs" component={About} />
-          <Route exact path="/contactUs" component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-        <Footer />
-      </div>
+      <AuthContext.Provider value={{
+        token: state.token,
+        userId: state.userId,
+        login: login,
+        logout: logout,
+      }}>
+        <div>
+          <Nav />
+          <Switch>
+            {!state.token && <Route exact path="/Home" component={Home} />}
+            {state.token && <Redirect from="/auth" to="/Home" exact />}
+            {state.token && <Route path="/" component={Home} exact/>}
+            {state.token && <Route path="/Home" component={Home} exact />}
+            {state.token && <Route path="/aboutUs" component={About} exact />}
+            {state.token && <Route path="/contactUs" component={Contact} exact />}
+           
+            {state.token && <Route path="/items" component={Items} exact />}
+            {state.token && <Route path="/item/:id" component={Detail} exact />}
+            {!state.token && <Route path="/aboutUs" component={About} exact />}
+            {!state.token && <Route path="/contactUs" component={Contact} exact/>}
+            {!state.token && <Route path="/auth" component={AuthPage} exact/>}
+            {!state.token && <Redirect from="/" to="/Home" exact />}
+            <Route component={NoMatch} />
+          </Switch>
+          <Footer />
+        </div>
+      </AuthContext.Provider>
     </Router>
   );
 }
