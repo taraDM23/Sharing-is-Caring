575edacb731410ab21ec595bbbc2a9329afca17d
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-28 23:29:25 +1100

    updated aboutus msg

--- client/src/pages/aboutUs.js
+++ client/src/pages/aboutUs.js
@@ -10,16 +10,7 @@ function aboutUs() {
         <h1>About Us</h1>
       </Jumbotron>
       <p style={{ marginRight: 50, marginLeft: 50, fontWeight: "normal",}}>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
-        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
-        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
-        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
-        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
-        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
+        This is a public site to post items that you have excess of or want to give away. 
       </p>  
     </div>
     
