37da4a744729bb42a5db5d0ecfc42c79d5a2bd31
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-30 16:58:30 +1100

    upadted contact us style

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,12 +6,14 @@ function contactUs() {
   return (
     <div>
       <Jumbotron>
-        <h1> Contact Us</h1>
+        
       </Jumbotron>
 
       <p style={{ marginRight: 50, marginLeft: 50, fontWeight: "normal", }}>
       </p>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7TC7u3tQ4NKYmmgHfDc1Hh51Ql5PdXmfA0Lc2ybVCH87gAg/viewform?embedded=true" width="640" height="2533" frameborder="0" marginheight="0" marginwidth="0" style={{ marginRight: 50, marginLeft: 150}}>Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7TC7u3tQ4NKYmmgHfDc1Hh51Ql5PdXmfA0Lc2ybVCH87gAg/viewform?embedded=true" 
+      width="100%" height="2533" frameborder="0" marginheight="0" marginwidth="0" 
+      style={{marginLeft: "0%"}}>Loading…</iframe>
       <p style={{ color: "white" }}>('\n')</p>
       <p style={{ color: "white" }}>('\n')</p>
       <p style={{ color: "white" }}>('\n')</p>
